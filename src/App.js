68a125aed4c45a06852714b9dcdfead587dca3c8
idanfadlon <66566962+idanfadlon@users.noreply.github.com>
2023-11-04 20:56:05 +0200

    finish theme settings and create 2 not implemented components (Sidebar and Topbar )

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { colorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
 function App() {
+  const [theme, colorMode] = useMode()
+
   return (
-    <div className="app"></div>
+    <colorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+    <div className="app">
+      <main className='content'></main>
+    </div>
+    </ThemeProvider>
+    </colorModeContext.Provider>
   );
 }
 
